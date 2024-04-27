8a03c90133574da3bc51a4cd8fd7a219db4bda23
shilaca <shilaca.fu@gmail.com>
2024-04-28 02:02:03 +0900

    add GTM

--- src/index.tsx
+++ src/index.tsx
@@ -2,5 +2,18 @@ import { render } from "solid-js/web";
 import { AppRoute } from "AppRoute";
 import "./style.css";
 
+/**
+ * Google Tag Manager
+ */
+((gtmId: string) => {
+  window.dataLayer = window.dataLayer || [];
+  window.dataLayer.push({ "gtm.start": new Date().getTime(), event: "gtm.js" });
+  const f = document.getElementsByTagName("script")[0];
+  const j = document.createElement("script");
+  j.async = true;
+  j.src = "https://www.googletagmanager.com/gtm.js?id=" + gtmId;
+  f.parentNode?.insertBefore(j, f);
+})(import.meta.env.VITE_GA_ID);
+
 const rootEl = document.getElementById("root");
 rootEl && render(() => <AppRoute />, rootEl);
---
+++ src/types/global.d.ts
@@ -0,0 +1,1 @@
+declare var dataLayer: Record<string, unknown>[] | undefined;
